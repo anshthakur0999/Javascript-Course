3d4f4ae36ef630cc27b07405367f178a9030e484
anshthakur0999 <80532812+anshthakur0999@users.noreply.github.com>
2025-02-01 14:26:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+/* alert("hello");*/
+// There's a different syntax for using alert in node js as we are not using browser
+
+console.log( 3
+    +
+    3);   //code redaibility should be high
+
+console.log("Ansh");
+
+let name = "Ansh";
+let age = 21;
+let isLoggedin = true;
+let state = null;
+
+// number=> 2 to power 53
+// bigInt
+// String => " ",''
+// Boolean => true/false
+// null => represntation of an empty value
+// undefined => Variable is declared but not assigned any value
+// symbol
+// object
+
+console.log(typeof age); // => type is number
+console.log(typeof null); // => type is object
+console.log(typeof undefined); // => typde is undefined

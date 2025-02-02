5d1beef36f02fdc23f4b56868009678871697d65
anshthakur0999 <80532812+anshthakur0999@users.noreply.github.com>
2025-02-02 10:33:36 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,61 @@
+let score = "32";
+//let score = false;
+//let score = null;
+//let score = undefined;
+//let score = "32abc";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = 12;
+let boolIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(typeof boolIsLoggedIn);
+//console.log(boolIsLoggedIn);
+
+// 1 => true    0 => false
+// "" => false   "Ansh" => true
+
+/****************************************************OPERATIONS*************************************************************/
+
+let value = 4;
+let negValue = -value;
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Ansh";
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);

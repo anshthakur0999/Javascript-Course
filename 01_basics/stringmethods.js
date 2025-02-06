0bc4a96b0dd91266627d6d808a1b4780044fc893
anshthakur0999 <80532812+anshthakur0999@users.noreply.github.com>
2025-02-06 15:07:32 +0000

    stringmethods

---
+++ 01_basics/stringmethods.js
@@ -0,0 +1,14 @@
+let name = 'Ansh';
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2,3));
+console.log(name.slice(2));
+
+let name2 = 'Harry';
+console.log(name2.concat(" is a friend of ",name));
+
+
+for(let i = 0; i < name.length; i++) {
+        console.log(name[i]);
+    }

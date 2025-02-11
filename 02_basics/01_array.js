ccb699e393836f66848c77477f2b83b84c7e1bcf
anshthakur0999 <80532812+anshthakur0999@users.noreply.github.com>
2025-02-11 14:32:17 +0000

    Arrays basic methods

---
+++ 02_basics/01_array.js
@@ -0,0 +1,54 @@
+const myArr = [0,1,2,3,4,5];
+//console.log(myArr[1]);
+//Javascript arrays are resizable and it can contain any datatype
+//zero indexed
+
+const myHeroes = ['shaktimaan','superman','thor','spiderman'];
+
+//Array can also be declared like given below
+const myArr2 = new Array(1,2,3,4);
+//console.log(myArr2[3]);
+
+//Array Methods
+
+//myArr2.push(5);
+//myArr2.push(6);
+//
+//console.log(myArr2);
+//myArr2.pop();
+//
+//console.log(myArr2);
+
+//myArr2.unshift(10);
+//console.log(myArr2);
+//
+//myArr2.shift();
+//console.log(myArr2);
+
+//console.log(myArr2.includes(9));
+//console.log(myArr2.indexOf(9));
+//console.log(myArr2.indexOf(3));
+
+//const newArr = myArr.join();
+//console.log(typeof myArr);
+//console.log(typeof newArr);
+
+//Slice and Splice
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+console.log("C ", myArr);
+
+
+
+
+
+
+
+
